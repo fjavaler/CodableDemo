de76e519b387eab136fdf10991eee72c73699cce
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 09:41:17 -0600

    Initial Commit

---
+++ CodableDemo/CodableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableDemoApp.swift
+//  CodableDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CodableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
